ac34cb381d075e1ef837ef4571c90883e555d39c
Leukhos <leukhos@fastmail.com>
2025-08-21 15:42:27 +0100

    Refactor benchmark functions to improve naming consistency and clarity

--- benchmarks/calculator.benchmark.cpp
+++ benchmarks/calculator.benchmark.cpp
@@ -4,44 +4,44 @@
 // Third-party headers
 #include <benchmark/benchmark.h>
 
-static void BM_Addition(benchmark::State &state) {
-  Calculator calc;
+static void BM_Calculator_Add_Basic(benchmark::State& state) {
+  Calculator calculator;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(calc.add(42, 17));
+    benchmark::DoNotOptimize(calculator.add(42, 17));
   }
 }
-BENCHMARK(BM_Addition);
+BENCHMARK(BM_Calculator_Add_Basic);
 
-static void BM_Subtraction(benchmark::State &state) {
-  Calculator calc;
+static void BM_Calculator_Subtract_Basic(benchmark::State& state) {
+  Calculator calculator;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(calc.subtract(42, 17));
+    benchmark::DoNotOptimize(calculator.subtract(42, 17));
   }
 }
-BENCHMARK(BM_Subtraction);
+BENCHMARK(BM_Calculator_Subtract_Basic);
 
-static void BM_Multiplication(benchmark::State &state) {
-  Calculator calc;
+static void BM_Calculator_Multiply_Basic(benchmark::State& state) {
+  Calculator calculator;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(calc.multiply(42, 17));
+    benchmark::DoNotOptimize(calculator.multiply(42, 17));
   }
 }
-BENCHMARK(BM_Multiplication);
+BENCHMARK(BM_Calculator_Multiply_Basic);
 
-static void BM_Division(benchmark::State &state) {
-  Calculator calc;
+static void BM_Calculator_Divide_Basic(benchmark::State& state) {
+  Calculator calculator;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(calc.divide(42, 17));
+    benchmark::DoNotOptimize(calculator.divide(42, 17));
   }
 }
-BENCHMARK(BM_Division);
+BENCHMARK(BM_Calculator_Divide_Basic);
 
-static void BM_AdditionRange(benchmark::State &state) {
-  Calculator calc;
+static void BM_Calculator_Add_Range(benchmark::State& state) {
+  Calculator calculator;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(calc.add(state.range(0), state.range(1)));
+    benchmark::DoNotOptimize(calculator.add(state.range(0), state.range(1)));
   }
 }
-BENCHMARK(BM_AdditionRange)->Args({8, 32})->Args({64, 128})->Args({512, 1024});
+BENCHMARK(BM_Calculator_Add_Range)->Args({8, 32})->Args({64, 128})->Args({512, 1024});
 
 BENCHMARK_MAIN();
