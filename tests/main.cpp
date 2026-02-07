2aaf7a4bf764afea7f31edec7fd0e03d922d5223
Leukhos <leukhos@fastmail.com>
2026-02-07 16:50:59 +0000

    Revert to googletest

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,8 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
